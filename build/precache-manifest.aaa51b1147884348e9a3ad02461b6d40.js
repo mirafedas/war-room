e15ef25c8296edf624e83964b7a899fdb0935b52
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-21 20:22:55 +0200

    deployment fixes part V

---
+++ build/precache-manifest.aaa51b1147884348e9a3ad02461b6d40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edc2d5d9186093efb86776f74635c0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5b4574a41cc4805717",
+    "url": "/static/css/main.9d59db81.chunk.css"
+  },
+  {
+    "revision": "b416224f2f369ff29544",
+    "url": "/static/js/2.f9809ffa.chunk.js"
+  },
+  {
+    "revision": "fa5b4574a41cc4805717",
+    "url": "/static/js/main.fd76c45d.chunk.js"
+  },
+  {
+    "revision": "498d2c36d0d92a935355",
+    "url": "/static/js/runtime-main.7a4026a5.js"
+  }
+]);
