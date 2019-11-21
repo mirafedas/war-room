beec9efc5ed9536646883926556c8e04777671f3
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-21 18:09:03 +0200

    fixed messages about joining the chat

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send('{"message" "Someone joined the chat"}');
+        client.send('{"message": "Someone joined the chat"}');
       }
     });
     
