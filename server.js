384dab4a012b3f5319ca083af76b8055f7266f61
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-21 13:25:35 +0200

    added notification for new user joining the chat

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send('{"message" "Someone joined the chat"}');
+      }
+    });
+    
+    ws.on('message', function incoming(data) {
     console.log(data);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
